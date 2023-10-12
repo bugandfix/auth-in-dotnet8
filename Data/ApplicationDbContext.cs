1e905d8db6bbd72b25c5a7172c2aa7989ae65ad7
Bug & Fix <139218056+bugandfix@users.noreply.github.com>
2023-10-12 23:26:01 +0330

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AuthinDotNet8.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; } = default!;
+    }
---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthinDotNet8.Models;
+
+public class Student
+{
+    [Key]
+    public int ID { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Family { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Address { get; set; }
+}
